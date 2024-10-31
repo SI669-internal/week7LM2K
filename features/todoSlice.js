b8b863c0456d1038a7b2a532663494fb63a414a1
Mullins, Benjamin <benm@umich.edu>
2024-10-31 14:21:41 -0400

    cln

--- features/todoSlice.js
+++ features/todoSlice.js
@@ -70,7 +70,6 @@ export const updateItemFromFirebase = createAsyncThunk(
   'todofirebase/updateItem',
   async ({item, inputText}) => {
     const newTodo = { ...item, text: inputText};
-    // let newTodos = todos.map(elem=>elem.key===todo.key?newTodo:elem);
     const doctoUpdate = doc(db, 'todos', todo.key);
     await updateDoc(doctoUpdate, {text: newText});
     return newTodo;
@@ -86,46 +85,8 @@ export const todoSlice = createSlice({
       { text: 'Get candy', key: Date.now() + 1}
     ],
   },
-  reducers: {
-    // addItem: (state, action) => {
-    //   // Redux Toolkit allows us to write "mutating" logic in reducers. It
-    //   // doesn't actually mutate the state because it uses the Immer library,
-    //   // which detects changes to a "draft state" and produces a brand new
-    //   // immutable state based off those changes.
-    //   // Also, no return statement is required from these functions.
-    //   const newListItem = {
-    //     text: action.payload,
-    //     key: Date.now() + Math.random()
-    //   };
-    //
-    //   state.value = [
-    //     ...state.value,
-    //     newListItem
-    //   ];
-    // },
-    updateItem: (state, action) => {
-        const {item, inputText} = action.payload
-      const newItem = {
-        text: inputText,
-        key: item.key
-      };
 
-      state.value = state.value.map(
-        elem => elem.key === newItem.key ? newItem : elem
-      )
-    },
-    // deleteItem: (state, action) => {
-    //   const itemId = action.payload.key
-    //   state.value = state.value.filter(elem=>elem.key !== itemId);
-    // },
-  },
   extraReducers: (builder) => {
-    builder.addCase(addItemPlusFirebase.pending, (state, action) => {
-      console.log('PRNDIN', action)
-    })
-    builder.addCase(addItemPlusFirebase.rejected, (state, action) => {
-      console.log('REJEc', action.error.message)
-    })
     builder.addCase(addItemPlusFirebase.fulfilled, (state, action) => {
       state.value = [
         ...state.value,
@@ -133,15 +94,9 @@ export const todoSlice = createSlice({
       ];
     })
     builder.addCase(getItemsFromFirebase.fulfilled, (state, action) => {
-      console.log("GET ITEMS PAYLOAD!", action.payload)
       state.value = action.payload
     })
-    builder.addCase(getItemsFromFirebase.pending, (state, action) => {
-      console.log("GET ITEMS Pending!", action)
-    })
-    builder.addCase(getItemsFromFirebase.rejected, (state, action) => {
-      console.log("GET ITEMS Rejactedr!", action)
-    })
+
     builder.addCase(deleteItemFromFirebase.fulfilled, (state, action) => {
       const itemId = action.payload.key
       state.value = state.value.filter(elem=>elem.key !== itemId);
